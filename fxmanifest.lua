dd54297e17e5f9f66ac8f697c03803050541945d
PRIMOIX <90275407+primoixi@users.noreply.github.com>
2022-12-31 20:36:32 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,17 +6,15 @@ description 'erratic_coke'
 version '1.0.0'
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
+    '@es_extended/locale.lua',
+    'locales/en.lua',
 	'config.lua',
     'client/erratic_coke_cl.lua'
 }
 
 server_scripts {
-	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
+    '@es_extended/locale.lua',
+    'locales/en.lua',
 	'config.lua',
     'server/erratic_coke_sv.lua'
 }
